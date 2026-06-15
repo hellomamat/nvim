f63b883252efc8c169ae73c74bd4a5d45f6190bc
rrahmatnn <rizkyrahmat198@gmail.com>
2026-06-15 15:41:45 +0700

    windows: hapus lua_ls dan jdtls

--- init_windows.lua
+++ init_windows.lua
@@ -263,12 +263,10 @@ require("lazy").setup({
 			require("mason-lspconfig").setup({
 				ensure_installed = {
 					"ts_ls",
-					"lua_ls",
 					"cssls",
 					"html",
 					"jsonls",
 					"gopls",
-					"jdtls",
 				},
 				automatic_installation = true,
 			})
@@ -304,20 +302,6 @@ require("lazy").setup({
 				on_attach = on_attach,
 			})
 
-			vim.lsp.config("lua_ls", {
-				cmd = { "lua-language-server" },
-				filetypes = { "lua" },
-				root_markers = { ".luarc.json", ".git" },
-				capabilities = capabilities,
-				on_attach = on_attach,
-				settings = {
-					Lua = {
-						diagnostics = { globals = { "vim" } },
-						workspace = { library = vim.api.nvim_get_runtime_file("", true) },
-					},
-				},
-			})
-
 			vim.lsp.config("gopls", {
 				cmd = { "gopls" },
 				filetypes = { "go", "gomod", "gowork", "gotmpl" },
@@ -351,71 +335,10 @@ require("lazy").setup({
 			})
 
 			vim.lsp.enable("ts_ls")
-			vim.lsp.enable("lua_ls")
 			vim.lsp.enable("gopls")
 			vim.lsp.enable("cssls")
 			vim.lsp.enable("html")
 			vim.lsp.enable("jsonls")
-		end,
-	},
-	{
-		"mfussenegger/nvim-jdtls",
-		ft = { "java" },
-		config = function()
-			local mason_path = vim.fn.stdpath("data") .. "/mason/packages/jdtls"
-			local launcher = vim.fn.glob(mason_path .. "/plugins/org.eclipse.equinox.launcher_*.jar")
-			local workspace = vim.fn.stdpath("data")
-				.. "/jdtls-workspace/"
-				.. vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
-
-			local config = {
-				cmd = {
-					"java",
-					"-Declipse.application=org.eclipse.jdt.ls.core.id1",
-					"-Dosgi.bundles.defaultStartLevel=4",
-					"-Declipse.product=org.eclipse.jdt.ls.core.product",
-					"-Dlog.level=ALL",
-					"-Xmx1g",
-					"--add-modules=ALL-SYSTEM",
-					"--add-opens", "java.base/java.util=ALL-UNNAMED",
-					"--add-opens", "java.base/java.lang=ALL-UNNAMED",
-					"-jar", launcher,
-					"-configuration", mason_path .. "/config_win",
-					"-data", workspace,
-				},
-				root_dir = require("jdtls.setup").find_root({
-					".git",
-					"mvnw",
-					"gradlew",
-					"pom.xml",
-					"build.gradle",
-				}),
-				settings = {
-					java = {
-						eclipse = { downloadSources = true },
-						maven = { downloadSources = true },
-						implementationsCodeLens = { enabled = true },
-						referencesCodeLens = { enabled = true },
-						format = { enabled = false },
-					},
-				},
-				on_attach = function(client, bufnr)
-					local opts = { noremap = true, silent = true, buffer = bufnr }
-					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-					vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-					vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-					vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-					vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-				end,
-			}
-
-			vim.api.nvim_create_autocmd("FileType", {
-				pattern = "java",
-				callback = function()
-					require("jdtls").start_or_attach(config)
-				end,
-			})
 		end,
 	},
 	{
